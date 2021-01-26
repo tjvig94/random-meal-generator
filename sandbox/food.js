652ada9a3ceb196517c9cdf464ede548325976e2
Patrick DeMarco <pfdemarco@hotmail.com>
2021-01-26 18:21:24 -0500

    added all countrys to a dropdown it works

--- sandbox/food.js
+++ sandbox/food.js
@@ -8,9 +8,10 @@ $(document).ready(function() {//dont forget this cause it will ruin your day
 
   function getFood(){
     // get the search text do some logic on this once it works and you have time
-    var sIn = $("#search-input").val();
+    var sIn = $("#country").val();
+    console.log(sIn);
     //put the api link here
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";//"https://www.thecocktaildb.com/api/json/v1/1/random.php"; //"https://www.themealdb.com/api/json/v1/1/categories.php"; //"https://www.themealdb.com/api/json/v1/1/filter.php?a=British";
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + sIn //https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"//"https://www.thecocktaildb.com/api/json/v1/1/random.php"; //"https://www.themealdb.com/api/json/v1/1/categories.php"; //"https://www.themealdb.com/api/json/v1/1/filter.php?a=British";
     
     //get the url info
     $.ajax({
