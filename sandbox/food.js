1472a2433e649070e8667bb33df9cd7524088bc7
Patrick DeMarco <pfdemarco@hotmail.com>
2021-01-26 16:53:59 -0500

    click button and inspect and look at console log of json

---
+++ sandbox/food.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {//dont forget this cause it will ruin your day
+
+  $("#search-button").on("click", function(Event){
+  
+    getFood();
+
+  });
+
+  function getFood(){
+    // get the search text do some logic on this once it works and you have time
+    var sIn = $("#search-input").val();
+    //put the api link here
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";//"https://www.thecocktaildb.com/api/json/v1/1/random.php"; //"https://www.themealdb.com/api/json/v1/1/categories.php"; //"https://www.themealdb.com/api/json/v1/1/filter.php?a=British";
+    
+    //get the url info
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+  })
+    
+    .then(function(response) {
+    // log it to see whats in it
+      console.log(response);
+    });
+  }
+})
