a12ef878deb65d05d4a990f8de51f50e0f4b35f5
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-27 20:48:18 -0500

    homepage layout basic

--- sandbox/food.js
+++ sandbox/food.js
@@ -1,29 +1,33 @@
-$(document).ready(function() {//dont forget this cause it will ruin your day
+$(document).ready(function () {//dont forget this cause it will ruin your day
 
-  $("#search-button").on("click", function(Event){
-  
+  $("#search-button").on("click", function (Event) {
+
     getFood();
 
   });
 
-  function getFood(){
+  function getFood() {
     // get the search text do some logic on this once it works and you have time
     var sIn = $("#country").val();
     console.log(sIn);
     //put the api link here
     var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + sIn //https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"//"https://www.thecocktaildb.com/api/json/v1/1/random.php"; //"https://www.themealdb.com/api/json/v1/1/categories.php"; //"https://www.themealdb.com/api/json/v1/1/filter.php?a=British";
-    
+
     //get the url info
     $.ajax({
       url: queryURL,
       method: "GET"
-  })
-    
-    .then(function(response) {
-    // log it to see whats in it
-      console.log(response);
-      $("#dispData").text(response.meals[0].strMeal);
-      $("#showMe").attr("src", response.meals[0].strMealThumb)
-    });
+    })
+
+      .then(function (response) {
+        // log it to see whats in it
+        console.log(response);
+        $("#dispData").text(response.meals[0].strMeal);
+        $("#showMe").attr("src", response.meals[0].strMealThumb)
+      });
   }
 })
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
