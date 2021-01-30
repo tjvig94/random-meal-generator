1fa636481b59a6e4f7d6bffdc34e4a7704085ff5
Patrick DeMarco <pfdemarco@hotmail.com>
2021-01-30 11:49:33 -0500

    added new id classes

--- sandbox/food.js
+++ sandbox/food.js
@@ -17,10 +17,12 @@ $(document).ready(function () {//dont forget this cause it will ruin your day
         // log it to see whats in it
         // console.log(response);
         $("#dispData").text(response.meals[0].strMeal);
+        $(".meal-name").text(response.meals[0].strMeal);
         window.localStorage.setItem("Meal" ,response.meals[0].strMeal);
         $("#showMe").attr("src", response.meals[0].strMealThumb);
+        $(".meal-img").attr("src", response.meals[0].strMealThumb);
         window.localStorage.setItem("Pic" ,response.meals[0].strMealThumb);
-        $("#ingreed").text(response.meals[0]);
+        $(".ingredients").text(response.meals[0]);
         window.localStorage.setItem("Ingred" ,response.meals[0]);
         getIngredients();
       });
@@ -42,6 +44,7 @@ $(document).ready(function () {//dont forget this cause it will ruin your day
         // log it to see whats in it
         console.log(response);
         $("#ingreed").text(response.meals[0].strInstructions);
+        $(".ingredients").text(response.meals[0].strInstructions);
       });
   }
 
